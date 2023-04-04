2bb48824e13098273c2c2b51da799b154bf7e8a9
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-04-04 16:55:39 +0900

    feat: 게시물 작성 및 저장 기능 구현

--- views/post_upload.py
+++ views/post_upload.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, url_for, render_template, flash, request
 from flask import redirect, session, g
 
+from datetime import datetime
+
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
@@ -9,8 +11,25 @@ db = client.dbjungle
 bp = Blueprint('post_upload', __name__, url_prefix='/post_upload')
 
 
-@bp.route('/', methods=('GET', 'POST'))
-def post_upload():   
+@bp.route('/', methods=('POST', 'GET'))
+def post_upload():
+    if request.method == 'POST':
+        post_title = request.form['post_title']
+        post_content = request.form['post_content']
+        post_file = request.form['post_file']
+
+        today = datetime.now()
+
+        new_post = {
+            'title': post_title,
+            'content': post_content,
+            'file': post_file,
+            'create_date': today,
+            'author': g.user['user_id']
+        }
+
+        res = db.post.insert_one(new_post)
+        print(new_post)
+        print(res)
+
     return render_template('post_upload.html')
-
-
