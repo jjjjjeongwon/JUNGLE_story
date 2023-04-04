a9295fcf1fdf951b17e016a35ae0c8dd391e4ea8
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-04-05 03:05:14 +0900

    feat: post업로드 이미지 진행중

--- views/post_detail.py
+++ views/post_detail.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, url_for, render_template, flash, request
+from flask import Blueprint, url_for, render_template, flash, request, jsonify
 from flask import redirect, session, g
 from datetime import datetime
+
+import gridfs
+
 
 from pymongo import MongoClient
 
@@ -19,11 +22,42 @@ def post_detail(create_date):
 @bp.route('/like/<create_date>', methods=('GET', 'POST'))
 def like(create_date):
     post = db.post.find_one({'create_date': create_date})
+
     user_id = g.user['user_id']
 
     if user_id not in post['like']:
         post['like'].append(user_id)
-        db.post.update_one({"create_date": create_date}, {"$set": {"like": post['like']}}, upsert=True)
+        db.post.update_one({"create_date": create_date}, {
+                           "$set": {"like": post['like']}}, upsert=True)
     else:
         flash("이미 좋아요를 누르셨습니다.")
     return render_template('post_detail.html', post=post)
+
+
+@bp.route('/delete/<create_date>')
+def delete_posts(create_date):
+
+    db.post.delete_one({'create_date': create_date})
+    return redirect(url_for('post_list.post_list'))
+
+
+@bp.route('/download/<create_date>', methods=('GET', 'POST'))
+def download_file(create_date):
+
+    fs = gridfs.GridFS(db)
+
+    post = db.post.find_one({'create_date': create_date})
+
+    outputdata = fs.get(post['file'])
+
+    # base64_img = codecs.encode(img_binary.read(), 'base64')
+
+    # decode_img = base64_img.decode('utf-8')
+
+    # post['file'] = decode_img
+
+    # my_id = data['_id']
+    # outputdata = fs.get(my_id).read()
+    output = open('./images/'+'back.jpeg', 'wb')
+    output.write(outputdata)
+    return jsonify({'msg': '저장에 성공했습니다.'})
